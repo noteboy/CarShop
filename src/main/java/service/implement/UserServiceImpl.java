918fad26ee2daf95aeb000f4680b00c25e272726
guangmingjian <1106697389@qq.com>
2017-05-19 20:28:38 +0800

    text

--- src/main/java/service/implement/UserServiceImpl.java
+++ src/main/java/service/implement/UserServiceImpl.java
@@ -1,7 +1,25 @@
 package service.implement;
 
+import model.CarEntity;
+import model.UserEntity;
+import service.UserService;
+
 /**
  * Created by Administrator on 2017/5/18 0018.
  */
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+    @Override
+    public boolean register(UserEntity user) {
+        return false;
+    }
+
+    @Override
+    public boolean login(UserEntity user) {
+        return false;
+    }
+
+    @Override
+    public CarEntity queryCar(String carId, String brank) {
+        return null;
+    }
 }
